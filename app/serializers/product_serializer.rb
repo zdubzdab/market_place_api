133570b3dbdee73e7b574f18123596fd2ddc6466
zdubzdab <zdubzdab@gmail.com>
2016-04-01 14:43:47 +0300

    Associates products and orders with a placements model(before 8.2)

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,9 @@
 class ProductSerializer < ActiveModel::Serializer
+  validates :title, :user_id, presence: true
   attributes :id, :title, :price, :published
 
   has_one :user
+
+  has_many :placements
+  has_many :orders, through: :placements
 end
