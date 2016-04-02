493b14d1af3cde21006a7f8f96cb31d5614975b6
zdubzdab <zdubzdab@gmail.com>
2016-04-02 12:01:48 +0300

    Adds a custom order product serializer to remove the user association

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,9 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  validates :title, :user_id, presence: true
   attributes :id, :title, :price, :published
 
   has_one :user
-
-  has_many :placements
-  has_many :orders, through: :placements
 end
--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -1,3 +1,4 @@
+
 require 'spec_helper'
 
 describe Api::V1::OrdersController do
@@ -22,13 +23,25 @@ describe Api::V1::OrdersController do
     before(:each) do
       current_user = FactoryGirl.create :user
       api_authorization_header current_user.auth_token
-      @order = FactoryGirl.create :order, user: current_user
+
+      @product = FactoryGirl.create :product
+      @order = FactoryGirl.create :order, user: current_user, product_ids: [@product.id]
       get :show, user_id: current_user.id, id: @order.id
     end
 
     it "returns the user order record matching the id" do
       order_response = json_response[:order]
       expect(order_response[:id]).to eql @order.id
+    end
+
+    it "includes the total for the order" do
+      order_response = json_response[:order]
+      expect(order_response[:total]).to eql @order.total.to_s
+    end
+
+    it "includes the products on the order" do
+      order_response = json_response[:order]
+      expect(order_response[:products]).to have(1).item
     end
 
     it { should respond_with 200 }
@@ -52,4 +65,5 @@ describe Api::V1::OrdersController do
 
     it { should respond_with 201 }
   end
+
 end
